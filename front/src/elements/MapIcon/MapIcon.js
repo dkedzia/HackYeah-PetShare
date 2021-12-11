bccc00f4f4eb260c783acc11625235e5efbe531c
Dominik Kędzia <dominikkedzia@gmail.com>
2021-12-11 23:57:43 +0100

    Improve map marker details

--- front/src/elements/MapIcon/MapIcon.js
+++ front/src/elements/MapIcon/MapIcon.js
@@ -4,12 +4,9 @@ delete L.Icon.Default.prototype._getIconUrl
 const customIcon = new L.Icon({
     iconUrl: gem,
     shadowUrl: null,
-
-    iconSize:     [50, 50], // size of the icon
-    // shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    // shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+    popupAnchor: [0, -25]
 })
 
 export default customIcon

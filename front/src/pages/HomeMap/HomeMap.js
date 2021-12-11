4b40d3d71e9f61b357452e19bd5acee88a592d6d
Dominik Kędzia <dominikkedzia@gmail.com>
2021-12-11 21:32:35 +0100

    Add map

--- front/src/pages/HomeMap/HomeMap.js
+++ front/src/pages/HomeMap/HomeMap.js
@@ -3,13 +3,26 @@ import "./home-map.scss"
 import Button from "../../components/Button/Button";
 import {Link} from "react-router-dom";
 import Paths from "../../Paths";
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import "react-leaflet"
+import "leaflet/dist/leaflet.css"
 
 class HomeMap extends React.Component
 {
     render() {
         return <div className="home-map">
             <div className="home-map__map-content">
-                Testowy content
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             </div>
             <div className="home-map__actions">
                 <Button content="Filtry"/>
